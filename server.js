d6ef1b50feecad573310fcb785eef55247ded74e
Nishant Joshi <nishantjoshi@Nishants-MacBook-Pro.local>
2023-12-04 08:53:40 +0545

    feat : hls video downloader

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.get('/convert', (req, res) => {
+    const hlsUrl = req.query.hlsUrl;
+    if (!hlsUrl) {
+        return res.status(400).send('No HLS URL provided');
+    }
+
+    const outputMp4 = path.join(__dirname, 'output.mp4');
+
+    ffmpeg(hlsUrl)
+        .outputOptions('-c copy')
+        .output(outputMp4)
+        .on('end', () => {
+
+            res.download(outputMp4, (err) => {
+                if (err) {
+                    console.error('Error in file download:', err);
+                }
+                fs.unlinkSync(outputMp4); 
+            });
+        })
+        .on('error', (err) => {
+            res.status(500).send('Error during conversion');
+        })
+        .run();
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
